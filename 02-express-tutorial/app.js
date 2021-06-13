d42d267fb1af37953fb71fda9c0c5d51c5a4d01e
itsmrkundu <itsmrkundu@gmail.com>
2021-06-13 11:52:36 +0530

    Basic of routing and query string

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,36 +1,6 @@
 const app = require('express')();
 const { products } = require('./data')
 
-app.get('/', (req, res) => {
-    res.send('<h1> Home Page</h1><a href="/api/products">products</a>');
-})
-
-app.get('/api/products', (req, res) => {
-    const newProd = products.map( product => {
-        const {id, name, image} = product;
-        return { id, name, image};
-    })
-    res.json(newProd)
-})
-// route parameter
-app.get('/api/products/:productID', (req, res) => {
-    //console.log(req.params);
-    const { productID } = req.params;
-    const singleProd = products.find( product => product.id === Number(productID));
-
-    if ( !singleProd ){
-        return res.status(404).send('Product Does Not Exist');
-    }
-
-    res.json(singleProd)
-})
-
-// complext route parameter
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    console.log(req.params);
-    res.send('Hello');
-})
-
 
 
 app.listen(5000, () => {
